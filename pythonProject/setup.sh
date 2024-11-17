04acea9f70086541f990fd98501da09521ca8742
samantashouvik49 <samantashouvik49@gmail.com>
2024-11-18 00:09:03 +0530

    Update setup.sh to include Git LFS setup

--- pythonProject/setup.sh
+++ pythonProject/setup.sh
@@ -7,3 +7,8 @@ enableCORS = false\n\
 headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
+
+# Install Git LFS and set it up
+apt update && apt install -y git-lfs   # Install Git LFS on Render
+git lfs install                        # Initialize Git LFS
+git lfs pull                           # Pull the large files tracked by LFS
